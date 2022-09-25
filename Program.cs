6a18382536cbe106bf8c917ab0df6f87aa757c88
mafik_tolik <mafik_tolik@mail.ru>
2022-09-25 03:00:34 +0300

    3 часа ночи, пора идти спать

--- Program.cs
+++ Program.cs
@@ -6,10 +6,15 @@
 // 12821 -> да
 // 23432 -> да
 
-Ex21();
+// Ex21();
 // Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Ex23();
+// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
 void Ex19()
 {
@@ -43,25 +48,45 @@ void Ex19()
 
 void Ex21()
 {
-
-    void input_coordinates(int x, int y, int z)
-    {
-        Console.Write("x:");
-        x = Convert.ToInt32(Console.ReadLine());
-        Console.Write("y:");
-        y = Convert.ToInt32(Console.ReadLine());
-        Console.Write("z:");
-        z = Convert.ToInt32(Console.ReadLine());
-    }
-
-    int x1 = 0, y1 = 0, z1 = 0;
-    int x2 = 0, y2 = 0, z2 = 0;
-
     Console.WriteLine("Введите координаты точки A:");
-    input_coordinates(x1, y1, z1);
+    Console.Write("x1: ");
+    int x1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("y1: ");
+    int y1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("z1: ");
+    int z1 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine($"A: ({x1}, {y1}, {z1})");
 
     Console.WriteLine("Введите координаты точки B:");
-    input_coordinates(x2, y2, z2);
+    Console.Write("x2: ");
+    int x2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("y2: ");
+    int y2 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("z2: ");
+    int z2 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine($"B: ({x2}, {y2}, {z2})");
+
+    Console.WriteLine("Для поиска расстояния АВ (x1,y1,z1 и x2,y2,z2) нам понадобится расстояние АС (x1,y1,z1 и x2,y1,z2) и расстояние ВС (x2,y2,z2 и x2,y1,z2)");
+    Console.WriteLine("Для поиска расстояния АС (x1,y1,z1 и x2,y1,z2) нам понадобится расстояние АD (x1,y1,z1 и x2,y1,z1) и расстояние CD (x2,y1,z2 и x2,y1,z1)");
+    Console.WriteLine($"С: ({x2}, {y1}, {z2})");
+    Console.WriteLine($"D: ({x2}, {y1}, {z1})");
+
+    int AD = x2 - x1;
+    int СD = z2 - z1;
+    int BC = y2 - y1;
+
+    Console.WriteLine("Расстояние между точками А и D = " + AD);
+    Console.WriteLine("Расстояние между точками C и D = " + СD);
+    Console.WriteLine("Расстояние между точками B и С = " + BC);
+
+    double AC = Math.Sqrt(Math.Pow(AD, 2) + Math.Pow(СD, 2));
+    Console.WriteLine("Расстояние между точками А и С = " + AC);
+
+    double АВ = Math.Sqrt(Math.Pow(AC, 2) + Math.Pow(BC, 2));
+    Console.WriteLine("Ответ: расстояние между точками А и В = " + АВ);
+}
+
+void Ex23()
+{
+    
 }
